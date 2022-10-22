48cddb907772be4eae95c18abb37d104a1ca8798
timmy-cde <71366703+timmy-cde@users.noreply.github.com>
2022-10-22 14:48:49 +0800

    Add files via upload

---
+++ collections.py
@@ -0,0 +1,33 @@
+# List
+myFruitList = ["apple", "banana", "cherry"]
+print(myFruitList)
+print(type(myFruitList))
+
+print(myFruitList[0])
+print(myFruitList[1])
+print(myFruitList[2])
+
+myFruitList[2] = "orange"
+print(myFruitList)
+
+# Tuple
+myFinalAnswerTuple = ("apple", "banana", "pinapple")
+print(myFinalAnswerTuple)
+print(type(myFinalAnswerTuple))
+
+print(myFinalAnswerTuple[0])
+print(myFinalAnswerTuple[1])
+print(myFinalAnswerTuple[2])
+
+# Dictionary
+myFavoriteDictionary = {
+    "Akua": "apple",
+    "Saanvi": "banana",
+    "Paulo": "pineapple"
+}
+print(myFavoriteDictionary)
+print(type(myFavoriteDictionary))
+
+print(myFavoriteDictionary["Akua"])
+print(myFavoriteDictionary["Saanvi"])
+print(myFavoriteDictionary["Paulo"])
---
+++ numeric-data.py
@@ -0,0 +1,29 @@
+print("Python has three numeric types: int, float, and complex")
+
+# Int 
+myValue = 1
+print(type(myValue))
+print(str(myValue) + " is of the data type " + str(type(myValue)))
+
+# Float
+myValue = 3.14
+print(myValue)
+print(type(myValue))
+print(str(myValue) + " is of the data type " + str(type(myValue)))
+
+# Complex
+myValue = 5j
+print(myValue)
+print(type(myValue))
+print(str(myValue) + " is of the data type " + str(type(myValue)))
+
+# Bool
+myValue = True
+print(myValue)
+print(type(myValue))
+print(str(myValue) + " is of the data type " + str(type(myValue)))
+
+myValue = False
+print(myValue)
+print(type(myValue))
+print(str(myValue) + " is of the data type " + str(type(myValue)))
